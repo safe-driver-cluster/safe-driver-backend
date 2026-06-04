947b7d1b52705156b4c7da3d6fd3b2f0f7b20cd9
ShaleelSandeepa <shaleelsandeepa7@gmail.com>
2026-06-05 02:11:38 +0530

    Update Sinhala voice alert messages and add evidence URL to behavior data

--- config/config.py
+++ config/config.py
@@ -307,7 +307,7 @@ VOICE_ALERT_FREQUENT_CLOSURES_L3_ENGLISH = 'Frequent eye closures detected! If y
 
 # Voice Alert Messages
 VOICE_ALERT_MICROSLEEP_SINHALA = 'සුළු මොහොතක නින්දක් (Microsleep) හඳුනාගෙන ඇත! කරුණාකර අවධානයෙන් සිටින්න.'
-VOICE_ALERT_YAWNING_SINHALA = 'හෑල්ලුම් යෑමක් (ඈනුම් ඇරීමක්) හඳුනාගෙන ඇත! කරුණාකර අවධානය යොමු කරන්න.'
+VOICE_ALERT_YAWNING_SINHALA = 'ඈනුම් යෑමක් හඳුනාගෙන ඇත! කරුණාකර අවධානය යොමු කරන්න.'
 VOICE_ALERT_DROWSY_SINHALA = 'නිදිමත ගතියක් හඳුනාගෙන ඇත! කරුණාකර සුළු විවේකයක් ගන්න.'
 VOICE_ALERT_DISTRACTION_SINHALA = 'රියදුරුගේ අවධානය ගිලිහී ඇති බව හඳුනාගෙන ඇත! කරුණාකර මාර්ගය වෙත අවධානය යොමු කරන්න.'
 VOICE_ALERT_HEAD_TURN_SINHALA = 'හිස හරවා බැලීමක් හඳුනාගෙන ඇත! කරුණාකර ඔබේ දෑස් මාර්ගය වෙතම යොමු කර තබාගන්න.'
--- database/db_helper.py
+++ database/db_helper.py
@@ -348,6 +348,7 @@ def save_behavior_to_firebase(mac: str, behavior_data: dict):
             'time': behavior_data.get('time', utils.now()),
             'number_plate': number_plate,
             'driver': behavior_data.get('driver', ''),
+            'evidence': 'https://firebasestorage.googleapis.com/v0/b/safe-driver-system.firebasestorage.app/o/evidence%2Fman-driving-car-700x400.jpg?alt=media&token=5f3e2648-be8c-42e8-b69d-61988dce8cd1'
         }
         
         # Save to latest
